68037f45c1fd9ad1e7a5f11186f15314d1a43eb4
Maks Birecki <maksymilian.birecki@gmail.com>
2024-09-25 19:57:28 +0200

    addActiveLinks

--- pierwszyreact/src/components/NavBar/NavBar.js
+++ pierwszyreact/src/components/NavBar/NavBar.js
@@ -1,13 +1,17 @@
 import styles from './NavBar.module.scss';
+import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
       <div className={styles.navbar}>
        <span className={styles.icon}><a href='/'><i className={'fa fa-tasks'}></i></a></span>
        <ul className={styles.list}>
-        <li><a href='/'>Home</a></li>
-        <li><a href='/favorite'>Favorite</a></li>
-        <li><a href='/about'>About</a></li>
+       <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}
+            to="/">Home</NavLink></li>
+        <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}
+            to="/favorite">Favorite</NavLink></li>
+        <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}
+            to="/about">About</NavLink></li>
        </ul>
       </div>
     );
